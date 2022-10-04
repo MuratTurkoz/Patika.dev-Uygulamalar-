670af00c21ea8c7f0bc6d30a56d68bc6c3d43a47
MuratTurkoz <44347576+MuratTurkoz@users.noreply.github.com>
2022-10-04 14:16:01 +0300

    Add files via upload

---
+++ Ders 1 Main Methodu - Namespace kavramı - System Using Direktifleri - Run/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("İsminizi giriniz");
+string name=Console.ReadLine();
+Console.WriteLine("Soyadınızı giriniz!");
+string lastName=Console.ReadLine();
+Console.WriteLine(name+" "+lastName);
+
+
