39b822e80dcf3699c54864aa834fb0b0830115d0
MuratTurkoz <44347576+MuratTurkoz@users.noreply.github.com>
2022-10-04 14:18:38 +0300

    Add files via upload

---
+++ Ders 4 Hata Yönetimi/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+try
+{
+    //kod blogu
+    int a = int.Parse(Console.ReadLine());
+
+    int b = int.Parse(Console.ReadLine());
+
+    int c = a+b;
+
+    Console.WriteLine(c);
+}
+catch(Exception ex)
+{
+    //Hata oldugunda çalışıcak blok
+    Console.WriteLine("Bir Hata Oluştu: "+ ex.Message.ToString());
+}
+finally
+{
+    //her zaman çalışacak blok
+    Console.WriteLine("İşlem tamamlandı.");
+}
+double am = 4.1;
+
+MathF.Round((float)am, 0);
+Console.WriteLine(MathF.Round((float)am, 1));
+Console.WriteLine(4.1.ToString());
+
+
+
+
+
+try
+{
+    int a = 300000000;
+}
+catch (ArgumentNullException ex)
+{
+
+    Console.WriteLine("Boş bir değer girdiniz: "+ ex);
+}
+catch (FormatException ex)
+{
+
+    Console.WriteLine("Veri tipi uygun değil: "+ ex);
+}
+catch (OverflowException ex)
+{
+
+    Console.WriteLine("Küçük ya da büyük bir değer girdiniz: "+ ex);
+}
+
+
