82e6bbeef7f18a80ad80a629cbc22ada873b479e
MuratTurkoz <44347576+MuratTurkoz@users.noreply.github.com>
2022-10-04 14:37:36 +0300

    Add files via upload

---
+++ Ders 15 Generic List/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ders_15_Generic_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //List<T> class
+            //System.Collections.Generic
+            //T=> object türünde
+            List<int> numberList = new List<int>() { 5, 3, 6, 9, 12, 78, 87, 983, 21 };
+            List<string> colorList = new List<string>() { "white", "pink", "red", "blue" };
+
+
+            foreach (int item in numberList)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (string item in colorList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("***********");
+            numberList.ForEach(x => Console.WriteLine(x));
+            colorList.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine("***********");
+
+            //Listeden eleman çıkarma
+
+            numberList.Remove(983);
+            colorList.Remove("white");
+            //Index ile sayı çıkarma
+            numberList.RemoveAt(5);
+            colorList.RemoveAt(2);
+            numberList.ForEach(x => Console.WriteLine(x));
+            colorList.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine("***********");
+            //Sayı listesinden arama
+            if (numberList.Contains(5))
+                Console.WriteLine("Yes");
+            //Eleman ile indexe erişme
+
+            Console.WriteLine(numberList.BinarySearch(21));
+
+            Console.WriteLine("***********");
+            //Diziyi Liste çevirme
+
+            string[] animals = { "dog", "cat", "monkey", "bird" };
+            List<string> animalList = new List<string>(animals);
+            animalList.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine("***********");
+
+            //Listeyi  temizleme
+            animalList.Clear();
+
+            //Liste içinde nesne tutma
+            List<Users> userList = new List<Users>() { new Users { Name = "Murat", SurName = "Türköz", Old = 35 }, new Users { Name = "Burçin", SurName = "Türköz", Old = 35 } };
+
+            userList.ForEach(x => Console.WriteLine(x.Name + " " + x.SurName + " " + x.Old));
+
+        }
+    }
+    class Users
+    {
+        private string name;
+        private string surName;
+        private int old;
+
+        public string Name { get => name; set => name = value; }
+        public string SurName { get => surName; set => surName = value; }
+        public int Old { get => old; set => old = value; }
+    }
+}
---
+++ Ders 16 ArrayList/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Ders_16_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList array = new ArrayList();
+            array.Add("Ali");
+            array.Add(12);
+            array.Add('a');
+            array.Add(5.12f);
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("********");
+            //AddRange
+            List<string> color = new List<string>() { "blue", "dark", "purple" };
+            int[] numbers = { 5, 3, 45, 78 };
+            array.AddRange(color);
+            array.AddRange(numbers);
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("********");
+            //Sort
+            // array.Sort();//hata tip uyuşmazlığı
+            Console.WriteLine("********");
+            //BinarySearch
+            //Önce sırakalam yapılması gerek
+            array.Clear();
+            array.AddRange(numbers);
+            array.Sort();
+            Console.WriteLine(array.BinarySearch(3));
+            Console.WriteLine("********");
+            //Reverse aynalama yapar
+            array.Reverse();
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
